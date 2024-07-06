b1bd1a4b0b335ea16000304e526d4f84e2204bd6
th-demi <mariademetrius6@gmail.com>
2024-07-06 20:11:42 +0530

    deployment code 7

--- Todo_list/settings.py
+++ Todo_list/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myapp/static/myapp/'),  # Adjust the path as per your directory structure
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 
 # Default primary key field type
